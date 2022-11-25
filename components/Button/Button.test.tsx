21160b21489c3c47c87eb933510d38799cff5f3d
tony <nguyenhuytoan1994@outlook.com>
2022-11-25 10:47:18 +0700

    11. Jest and React Testing Library

---
+++ components/Button/Button.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Button from "./Button";
+
+describe("Button test cases", () => {
+  it("Render check", () => {
+    const onClick = jest.fn();
+    const { asFragment } = render(<Button onClick={onClick}>Button</Button>);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("Check onClick callback", async () => {
+    const onClick = jest.fn();
+
+    render(<Button onClick={onClick}>Button</Button>);
+
+    const element = screen.getByRole("button");
+
+    await userEvent.click(element);
+
+    expect(onClick).toHaveBeenCalled();
+  });
+});
