365e3d67bb75a707fe80dc600959731fb0fc8788
tony <nguyenhuytoan1994@outlook.com>
2022-12-01 00:29:13 +0700

    14. Aliases and themes for Jest

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: [
     "../pages/**/*.stories.mdx",
@@ -5,13 +7,27 @@ module.exports = {
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  // https://github.com/storybookjs/storybook/issues/10231
+  features: {
+    emotionAlias: false,
   },
 };
