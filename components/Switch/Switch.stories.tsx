dcf5b9dc07f65bbb19d67b43116f10c79dad5e22
tony <nguyenhuytoan1994@outlook.com>
2022-12-09 15:59:50 +0700

    24. Switch component

---
+++ components/Switch/Switch.stories.tsx
@@ -0,0 +1,16 @@
+import { expect } from '@storybook/jest'
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { screen, userEvent } from '@storybook/testing-library'
+import { Switch } from './Switch'
+
+export default {
+  title: 'Controls/Switch',
+  component: Switch,
+} as ComponentMeta<typeof Switch>
+
+export const BasicSwitch: ComponentStoryObj<typeof Switch> = {
+  play: async ({ args }) => {
+    await userEvent.click(screen.getByTestId('SwitchVisiblePart'))
+    await expect(args.onChange).toHaveBeenCalled()
+  },
+}
---
+++ components/Switch/Switch.test.tsx
@@ -0,0 +1,25 @@
+import { expect } from '@storybook/jest'
+import userEvent from '@testing-library/user-event'
+import { render, screen } from 'test-utils'
+import { Switch } from './Switch'
+
+describe('Switch test cases', () => {
+  it('Render check', () => {
+    const onChange = jest.fn()
+    jest.spyOn(Math, 'random').mockReturnValue(0.99999999)
+
+    const { asFragment } = render(<Switch onChange={onChange} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+  it('Check onChange callback', async () => {
+    const onChange = jest.fn()
+
+    render(<Switch onChange={onChange} />)
+
+    const element = screen.getByTestId('SwitchVisiblePart')
+
+    await userEvent.click(element)
+
+    expect(onChange).toHaveBeenCalled()
+  })
+})
